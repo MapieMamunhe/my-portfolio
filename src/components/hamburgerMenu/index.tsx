74fa85902df5b9d0939d3c48bc22189928b5b414
Mapie Mamunhe <mmamunhe@gmail.com>
2022-11-14 20:59:01 +0200

    Add component styles for hover effect

--- src/components/hamburgerMenu/index.tsx
+++ src/components/hamburgerMenu/index.tsx
@@ -5,7 +5,10 @@ import React from "react";
 const HamburgerMenu: React.FC = () => {
   return (
     <>
-      <div className="space-y-2 flex flex-col items-end ">
+      <div
+        className="space-y-2 flex flex-col items-end hamburger w-18 h-9 justify-center px-2 mx-2"
+        style={{}}
+      >
         <span className="w-14 h-0.5 bg-white"></span>
         <span className="w-10 h-0.5 bg-white"></span>
         <span className="w-6 h-0.5 bg-white"></span>
